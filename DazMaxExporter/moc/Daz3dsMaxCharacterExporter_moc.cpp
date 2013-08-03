fe222e16475502fd14fda48d3418842e2c3c7bb5
Sebastian Friston <sebastian.friston.12@ucl.ac.uk>
2013-08-03 20:39:00 +0100

    started renaming dazexporter project...

--- DazMaxExporter/moc/Daz3dsMaxCharacterExporter_moc.cpp
+++ DazMaxExporter/moc/Daz3dsMaxCharacterExporter_moc.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'Daz3dsMaxCharacterExporter.h'
 **
-** Created: Thu 1. Aug 20:56:24 2013
+** Created: Sat 3. Aug 20:37:56 2013
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.1)
 **
 ** WARNING! All changes made in this file will be lost!
